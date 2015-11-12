835a33809ac2dd008b414cbc554521a2a7fd9e70
Prakash Joshi <prakashjoshiversion1@gmail.com>
2015-11-12 08:37:52 +0545

    hot reload added

--- src/main/java/com/test/vertBlog/ServVerticle.java
+++ src/main/java/com/test/vertBlog/ServVerticle.java
@@ -3,7 +3,9 @@ package com.test.vertBlog;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.http.impl.HttpServerImpl;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
 
 /**
  * Created by pcjoshi on 11/11/15.
@@ -18,6 +20,8 @@ public class ServVerticle extends AbstractVerticle {
             response.putHeader("content-type","text/html")
                     .end("<h1>Hello from my first Vert.x 3 application</h1>");
         });
+
+        router.route("/assets/*").handler(StaticHandler.create("assets"));
 
         vertx.createHttpServer().requestHandler(router::accept).listen(config().getInteger("http.port",8080),result -> {
             if (result.succeeded()){
